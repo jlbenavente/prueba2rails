42dddc88e2765b47c4b5efc5223ff309abcfe798
jlbenavente <jlbenaventecanales@gmail.com>
2019-01-24 21:13:33 -0300

    actividades realizadas

--- app/controllers/usertasks_controller.rb
+++ app/controllers/usertasks_controller.rb
@@ -1,11 +1,11 @@
 class UsertasksController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
   	@task = Task.find(params[:task_id])
   	@usertask = Usertask.new(task: @task, user: current_user)
   	if @usertask.save
-  		redirect_to tasks_path, notice: 'Has indicado que has realizado estaactividad'
+  		redirect_to tasks_path, notice: 'Has indicado que has realizado esta actividad'
   	else
   		redirect_to tasks_path alert: 'No se ha podido ingresar su orden'
   	end
