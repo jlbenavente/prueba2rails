9405c8475b049b227a23cb9a6a472c7474eacacc
jlbenavente <42591168+jlbenavente@users.noreply.github.com>
2019-01-27 08:05:21 -0300

    Add files via upload

--- app/controllers/usertasks_controller.rb
+++ app/controllers/usertasks_controller.rb
@@ -10,6 +10,17 @@ class UsertasksController < ApplicationController
   		redirect_to tasks_path alert: 'No se ha podido ingresar su orden'
   	end
   end
+  def delete
+      def create
+    @task = Task.find(params[:task_id])
+    @usertask = Usertask.delete(task: @task, user: current_user)
+    if @usertask.save
+      redirect_to tasks_path, notice: 'Has indicado que has realizado esta actividad'
+    else
+      redirect_to tasks_path alert: 'No se ha podido ingresar su orden'
+    end
+  end
+  end
 
   def index
   	@usertask = current_user.usertasks
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-	has_many :usertasks
+  has_many :usertasks
   has_many :users, through: :usertasks
 end
